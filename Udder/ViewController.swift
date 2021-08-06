53b91d001e6b7f525d6ba098f5e221f04b052a4b
Yurii <->
2021-08-06 18:06:15 +0300

    2.0 Version

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Yurii Filozop on 06.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    khbvhgvuyty iuygiuygb oijhb oi
+}
+
